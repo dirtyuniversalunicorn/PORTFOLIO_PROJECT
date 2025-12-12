b2528fbc4e1678c3746d193c6293553a30094989
Jaro <jaroslavfilo95@gmail.com>
2025-12-12 18:12:48 +0100

    adjust paddingX of Button

--- src/components/Button.tsx/index.tsx
+++ src/components/Button.tsx/index.tsx
@@ -3,7 +3,13 @@ import { Button as ChakraButton } from "@chakra-ui/react";
 
 export const Button = ({ buttonTitle, ...props }: ButtonProps) => {
   return (
-    <ChakraButton textTransform="uppercase" fontWeight={400} {...props}>
+    <ChakraButton
+      textTransform="uppercase"
+      fontWeight={400}
+      {...props}
+      borderRadius={0}
+      paddingX={7}
+    >
       {buttonTitle}
     </ChakraButton>
   );
