a2609ff0b80f9ed4c99fa8c239e347c4fdd106ad
Jaro <jaroslavfilo95@gmail.com>
2025-12-12 18:06:27 +0100

    create Band component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Band } from "@/components/Band";
 import { Title } from "@/components/Title";
 import { Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Flex id="landing_page_section" justify="center" align="end">
+    <Flex id="landing_page_section" flexDirection="column">
+      <Band />
       <Title title="Jaroslav Fiľo" />
     </Flex>
   );
---
+++ src/components/Band.tsx
@@ -0,0 +1,25 @@
+import { Flex, Text, Wrap } from "@chakra-ui/react";
+
+export const Band = () => {
+  return (
+    <Flex
+      alignSelf="center"
+      justifyContent={{ md: "space-around" }}
+      flexDirection={{ base: "column", md: "row" }}
+      gap={{ base: 10, md: 0 }}
+      width={{ base: "95%", md: "100%" }}
+      mt="auto"
+      mx="auto"
+      ml={{ base: "5%", md: 0 }}
+      fontSize={{ base: "xl" }}
+      textTransform="uppercase"
+      fontWeight={100}
+      letterSpacing={2}
+      opacity={0.7}
+    >
+      <Text>Developer</Text>
+      <Text>Innovator</Text>
+      <Text>Olomouc, CZECH REPUBLIC</Text>
+    </Flex>
+  );
+};
