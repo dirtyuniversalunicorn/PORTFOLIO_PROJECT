e8a3e8e4632e8da3a648012bda740d89c5c056cd
Jaro <jaroslavfilo95@gmail.com>
2025-12-12 23:33:52 +0100

    setup grid for footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,18 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Grid } from "@chakra-ui/react";
 
 export const Footer = () => {
-  return <Flex id="footer_section">Footer</Flex>;
+  return (
+    <Grid id="footer_section" gridTemplateRows="repeat(2, 1fr)">
+      <Grid gridTemplateColumns="repeat(2, 1fr)">
+        <Box>1</Box>
+        <Box>2</Box>
+      </Grid>
+      <Grid gridTemplateColumns="repeat(4, 1fr)">
+        <Box>3</Box>
+        <Box>4</Box>
+        <Box>5</Box>
+        <Box>6</Box>
+      </Grid>
+    </Grid>
+  );
 };
