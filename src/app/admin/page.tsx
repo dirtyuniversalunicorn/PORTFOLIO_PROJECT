4a645764da616171b72d902324a289a0f5c37ad2
Jaro <jaroslavfilo95@gmail.com>
2025-12-14 16:35:31 +0100

    get dynamic data from db

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,14 +2,24 @@
 // pretty much content management system
 
 import { UploadProject } from "@/components/UploadProject";
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
+import prisma from "../../../lib/prisma";
 
 export default async function Admin() {
+  const projects = await prisma.project.findMany();
   return (
     <Box as="section">
       <h1>Admin page</h1>
       <Box mt={96} maxWidth={1400} mx="auto">
         <UploadProject />
+        {projects.map((project) => (
+          <Box key={project.id}>
+            <Text>Title: {project.title}</Text>
+            <Text>Short description: {project.shortDescription}</Text>
+            <Text>Long description: {project.longDescription}</Text>
+            {/* <Text>Technologies: {project.title}</Text> */}
+          </Box>
+        ))}
       </Box>
     </Box>
   );
