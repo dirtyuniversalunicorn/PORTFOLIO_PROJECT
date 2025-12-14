8594bc05946413ffb0cdbe0f0ff2ada75abd5fe7
Jaro <jaroslavfilo95@gmail.com>
2025-12-14 15:04:39 +0100

    TODO function for generating slugs

---
+++ src/utils/generateSlug.ts
@@ -0,0 +1,3 @@
+// TODO
+// function for creating slug name of the dynamicaly generated page
+// take the title of the project, remove all diacritics, and replace all the spaces with dashes, toLowerCase, no special characters: !:_? etc
