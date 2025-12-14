99b6b0e8f924fcef252958936d6629b43b1192ba
Jaro <jaroslavfilo95@gmail.com>
2025-12-14 15:46:46 +0100

    create test component for Upload

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,16 @@
 // TODO - this will be protected path
 // pretty much content management system
 
+import { UploadProject } from "@/components/UploadProject";
+import { Box } from "@chakra-ui/react";
+
 export default async function Admin() {
-  return <h1>Admin page</h1>;
+  return (
+    <Box as="section">
+      <h1>Admin page</h1>
+      <Box mt={96} maxWidth={1400} mx="auto">
+        <UploadProject />
+      </Box>
+    </Box>
+  );
 }
