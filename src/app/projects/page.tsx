683307777de2c99833d71cc117d373d7affd2648
Jaro <jaroslavfilo95@gmail.com>
2025-12-15 22:10:47 +0100

    render projects on /projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,31 @@
+import { ProjectList } from "@/components/ProjectsSection/ProjectList";
+import prisma from "@/lib/prisma";
+import { Stack, Text } from "@chakra-ui/react";
+
+// TODO types
+
 export default async function Projects() {
-  return <h1>Projects page</h1>;
+  const projects = await prisma.project.findMany();
+  return (
+    <Stack
+      as="section"
+      py={40}
+      gap={10}
+      maxWidth={1400}
+      mx={{ base: "5%", lg: "auto" }}
+    >
+      <Text
+        fontSize="7xl"
+        opacity={0.05}
+        textAlign="right"
+        zIndex={1}
+        position="absolute"
+        right={0}
+        mx={"5%"}
+      >
+        Projects
+      </Text>
+      <ProjectList projects={projects} limit="all" />
+    </Stack>
+  );
 }
