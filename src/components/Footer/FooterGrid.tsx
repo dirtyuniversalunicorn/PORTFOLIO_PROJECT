4cc9f2b32d087faa344dfa50a37db98d8856a61e
Jaro <jaroslavfilo95@gmail.com>
2026-01-06 21:43:58 +0100

    replace custom icons for react icons

--- src/components/Footer/FooterGrid.tsx
+++ src/components/Footer/FooterGrid.tsx
@@ -1,48 +1,42 @@
-import { Grid, Box, GridItem } from "@chakra-ui/react";
-import { FooterGridList } from "./FooterGridList";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import { footerNecessities } from "@/constants/footerNecessities";
 import { footerPages } from "@/constants/footerPages";
-import { Socials } from "../Socials";
 import { socials } from "@/constants/socials";
+import { Socials } from "../Socials";
+import { FooterGridList } from "./FooterGridList";
 
 export const FooterGrid = () => {
-    return (
-        <Grid
-            templateColumns={{
-                base: "repeat(2, 1fr)",
-                sm: "repeat(2, 1fr)",
-                md: "repeat(3, 1fr)",
-                lg: "repeat(4, 1fr)",
-            }}
-            templateRows={{
-                base: "repeat(2, auto)",
-                sm: "repeat(2, auto)",
-                md: "repeat(1, auto)",
-            }}
-            gap={{ base: "10", md: "28" }}
-            justifyItems={{ base: "center" }}
-        >
-            <GridItem colSpan={{ base: 2, md: 1 }}>
-                <Socials
-                    showTitle
-                    socials={socials}
-                    display="flex"
-                    flexDirection={{ base: "row", md: "column" }}
-                />
-            </GridItem>
-            <GridItem
-                colStart={{ base: 1, md: 2 }}
-                rowStart={{ base: 2, md: 1 }}
-            >
-                <FooterGridList footerPages={footerPages} />
-            </GridItem>
-            <GridItem
-                colStart={{ base: 2, md: 3 }}
-                rowStart={{ base: 2, md: 1 }}
-            >
-                <FooterGridList footerPages={footerNecessities} />
-            </GridItem>
-            <Box display={{ base: "none", lg: "block" }} />
-        </Grid>
-    );
+  return (
+    <Grid
+      templateColumns={{
+        base: "repeat(2, 1fr)",
+        sm: "repeat(2, 1fr)",
+        md: "repeat(3, 1fr)",
+        lg: "repeat(4, 1fr)",
+      }}
+      templateRows={{
+        base: "repeat(2, auto)",
+        sm: "repeat(2, auto)",
+        md: "repeat(1, auto)",
+      }}
+      gap={{ base: "10", md: "28" }}
+      justifyItems={{ base: "center" }}
+    >
+      <GridItem colSpan={{ base: 2, md: 1 }}>
+        <Socials
+          showTitle
+          socials={socials}
+          display="flex"
+          flexDirection={{ base: "row", md: "column" }}
+        />
+      </GridItem>
+      <GridItem colStart={{ base: 1, md: 2 }} rowStart={{ base: 2, md: 1 }}>
+        <FooterGridList footerPages={footerPages} />
+      </GridItem>
+      <GridItem colStart={{ base: 2, md: 3 }} rowStart={{ base: 2, md: 1 }}>
+        <FooterGridList footerPages={footerNecessities} />
+      </GridItem>
+      <Box display={{ base: "none", lg: "block" }} />
+    </Grid>
+  );
 };
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,18 +1,28 @@
-import { Flex, Image, Link, Text, Wrap } from "@chakra-ui/react";
+import { Flex, Link, Text, Wrap } from "@chakra-ui/react";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 import type { SocialsProps } from "@/types/AboutSectionProps";
 
 export const Socials = ({ socials, showTitle, ...props }: SocialsProps) => {
+  const getIcon = (name: string) => {
+    const lowerName = name.toLowerCase();
+
+    if (lowerName === "github") {
+      return <FaGithub size={35} />;
+    } else if (lowerName === "facebook") {
+      return <FaFacebook size={35} />;
+    } else if (lowerName === "linkedin") {
+      return <FaLinkedin size={35} />;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <Wrap order={{ base: 1, md: 2 }} mx={{ base: "auto", md: 0 }} {...props}>
       {socials.map((social) => (
         <Flex key={social.index} alignItems="center" gap={2}>
           <Link href={social.url} target="_blank">
-            <Image
-              width="40px"
-              objectFit="contain"
-              src={social.pictureUrl}
-              alt={social.name}
-            />
+            {getIcon(social.name)}
             {showTitle && (
               <Text display={{ base: "none", md: "block" }}>{social.name}</Text>
             )}
