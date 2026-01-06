b5974f8929319ff63e6b9a928009e329945b3afb
Jaro <jaroslavfilo95@gmail.com>
2026-01-06 20:59:34 +0100

    make links from social icons

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,4 +1,4 @@
-import { Clipboard, Flex, Image, Link, Text, Wrap } from "@chakra-ui/react";
+import { Flex, Image, Link, Text, Wrap } from "@chakra-ui/react";
 import type { SocialsProps } from "@/types/AboutSectionProps";
 
 export const Socials = ({ socials, showTitle, ...props }: SocialsProps) => {
@@ -6,23 +6,17 @@ export const Socials = ({ socials, showTitle, ...props }: SocialsProps) => {
     <Wrap order={{ base: 1, md: 2 }} mx={{ base: "auto", md: 0 }} {...props}>
       {socials.map((social) => (
         <Flex key={social.index} alignItems="center" gap={2}>
-          <Clipboard.Root value={social.url}>
-            <Clipboard.Trigger asChild>
-              <Link>
-                <Image
-                  width="40px"
-                  objectFit="contain"
-                  src={social.pictureUrl}
-                  alt={social.name}
-                />
-                {showTitle && (
-                  <Text display={{ base: "none", md: "block" }}>
-                    {social.name}
-                  </Text>
-                )}
-              </Link>
-            </Clipboard.Trigger>
-          </Clipboard.Root>
+          <Link href={social.url} target="_blank">
+            <Image
+              width="40px"
+              objectFit="contain"
+              src={social.pictureUrl}
+              alt={social.name}
+            />
+            {showTitle && (
+              <Text display={{ base: "none", md: "block" }}>{social.name}</Text>
+            )}
+          </Link>
         </Flex>
       ))}
     </Wrap>
