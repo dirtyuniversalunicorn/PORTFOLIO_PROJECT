94c7f5edaed04ea550d053387d157923a2fd6948
Jaro <jaroslavfilo95@gmail.com>
2026-01-18 22:03:41 +0100

    add use cache directive

--- src/components/Education/Certificates.tsx
+++ src/components/Education/Certificates.tsx
@@ -1,14 +1,15 @@
-import prisma from "@/lib/prisma";
-import { CertificatesClient } from "./CertificatesClient";
-import { sleep } from "@/utils/sleep";
 import type { Certificate } from "@/generated/prisma/client";
-import { SectionSkeleton } from "../Skeletons/SkeletonSection";
+import prisma from "@/lib/prisma";
+import { sleep } from "@/utils/sleep";
 import { SkeletonCertificates } from "../Skeletons/SkeletonCertificates";
+import { SectionSkeleton } from "../Skeletons/SkeletonSection";
+import { CertificatesClient } from "./CertificatesClient";
 
 // TODO fix types
 // is there way to not double the information, since the same enum is used in prisma.schema
 
 export const Certificates = async () => {
+  "use cache";
   let certificates: Certificate[] = [];
   try {
     certificates = await prisma.certificate.findMany();
