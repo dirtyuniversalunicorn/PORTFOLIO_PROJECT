6bdaacf5b05d748140dbc1e864f7e97a96f733f8
Jaro <jaroslavfilo95@gmail.com>
2026-01-24 10:42:00 +0100

    styles of long and short description on project detail

--- src/app/projects/[slug]/components/ProjectCharacteristics.tsx
+++ src/app/projects/[slug]/components/ProjectCharacteristics.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import {
+  Box,
+  Collapsible,
+  HStack,
+  Stack,
+  Text,
+  VStack,
+  Wrap,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import { DisplayBadges } from "@/components/Badges";
 import { IconTooltip } from "@/components/IconTooltip";
 import type { ProjectProps } from "@/types/ProjectsProps";
-import { Stack, HStack, Wrap, Text, Collapsible, Box } from "@chakra-ui/react";
-import { useState } from "react";
-import { FaExternalLinkAlt } from "react-icons/fa";
 
 type ProjectCharacteristics = {
   projectDetails: ProjectProps;
@@ -17,27 +25,27 @@ export const ProjectCharacteristics = ({
   const [isOpen, setIsOpen] = useState(false);
   return (
     <Stack>
-      <HStack gap={4}>
-        <Text as="h1" fontSize={{ base: "5xl" }}>
+      <VStack gap={4} alignItems="start">
+        <Text as="h1" fontSize={{ base: "5xl" }} lineHeight={1.3}>
           {projectDetails?.title}
         </Text>
-      </HStack>
-      <Wrap>
-        <IconTooltip
-          content="Visit project"
-          link={projectDetails.websiteUrl}
-          icon={<FaExternalLinkAlt />}
-        />
-        <DisplayBadges badgeData={projectDetails.technologies} />
-      </Wrap>
-      <Text>{projectDetails?.shortDescription}</Text>
+        <Wrap>
+          <IconTooltip
+            content="Visit project"
+            link={projectDetails.websiteUrl}
+            icon={<FaExternalLinkAlt />}
+          />
+          <DisplayBadges badgeData={projectDetails.technologies} />
+        </Wrap>
+      </VStack>
+      <Text textAlign="justify">{projectDetails?.shortDescription}</Text>
       <Collapsible.Root open={isOpen} onOpenChange={(e) => setIsOpen(e.open)}>
-        <Collapsible.Trigger paddingY="3">
-          {isOpen ? "Hide..." : "Read more..."}
+        <Collapsible.Trigger paddingY="3" color="gray.500">
+          {isOpen ? "Hide ..." : "More ..."}
         </Collapsible.Trigger>
         <Collapsible.Content>
           <Box>
-            <Text>{projectDetails?.longDescription}</Text>
+            <Text textAlign="justify">{projectDetails?.longDescription}</Text>
           </Box>
         </Collapsible.Content>
       </Collapsible.Root>
